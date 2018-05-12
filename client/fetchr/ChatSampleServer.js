2fd8a4a4b649d6c136de296226210c25677217a5
MaxMEllon <maxmellon1994@gmail.com>
2018-05-12 20:49:45 +0900

    Refactoring

--- client/fetchr/ChatSampleServer.js
+++ client/fetchr/ChatSampleServer.js
@@ -1,8 +1,14 @@
+// @flow
+
 import axios from 'axios'
+import type { Rooms } from '../types'
+
+// FIXME: this variable should be desided by environment
+const baseURL = 'http://localhost:4000'
 
 export default {
-  getRooms() {
-    const url = `http://localhost:4000/rooms`
+  getRooms(): Promise<Rooms> {
+    const url = `${baseURL}/rooms`
     return new Promise((resolve, reject) => {
       axios
         .get(url)
